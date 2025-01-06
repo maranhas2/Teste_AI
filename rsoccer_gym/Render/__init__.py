6b643f80dd8ad09b89b648b24f1ea208d3ecb530
maranhas2 <rodrigo.maranhao2@gmail.com>
2025-01-06 09:22:24 -0300

    Removendo rSoccer de dentro dos arquivos

--- rsoccer_gym/Render/__init__.py
+++
@@ -1,4 +0,0 @@
-from rsoccer_gym.Render.ball import Ball
-from rsoccer_gym.Render.field import Sim2DRenderField, SSLRenderField, VSSRenderField
-from rsoccer_gym.Render.robot import Sim2DRobot, SSLRobot, VSSRobot
-from rsoccer_gym.Render.utils import COLORS
--- rsoccer_gym/ssl/ssl_hw_challenge/__init__.py
+++
@@ -1,4 +0,0 @@
-from rsoccer_gym.ssl.ssl_hw_challenge.static_defenders import SSLHWStaticDefendersEnv
-from rsoccer_gym.ssl.ssl_hw_challenge.dribbling import SSLHWDribblingEnv
-from rsoccer_gym.ssl.ssl_hw_challenge.contested_possession import SSLContestedPossessionEnv
-from rsoccer_gym.ssl.ssl_hw_challenge.pass_endurance import SSLPassEnduranceEnv
